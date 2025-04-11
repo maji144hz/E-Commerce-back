deeafedbedb1cfaff51e67c093ace5e6862a87b6
maji1106 <nnnpphotmail.com@gmail.com>
2025-04-12 00:11:46 +0700

    FIX

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,8 +54,10 @@ const Navbar = () => {
           </ul>
         </div>
         <a className="btn btn-ghost text-xl flex items-center">
-          <img src="/bg.png" alt="" className="mr-2 h-8" />
-          SE Souvenir Shop
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 text-red-500">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+          </svg>
+          <span className="ml-2 font-bold">SE Shop</span>
         </a>
       </div>
       <div className="navbar-center hidden lg:flex">
